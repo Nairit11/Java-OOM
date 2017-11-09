cf703673bb5b48253026ec65047b38ef58a25264
Nairit Banerjee <nairit32@gmail.com>
2017-11-09 16:51:28 +0530

    Uploaded revised code

--- Main.java
+++ Main.java
@@ -54,49 +54,58 @@ class student {
     private course[] courses;
     private int c;
     private double sum,sumcr;
-    public student(String rollNo) throws InvalidRollNoException{
-	String sub1=rollNo.substring(0,3);
-	String sub2=rollNo.substring(6,7);
-	int sub3=Integer.parseInt(rollNo.substring(7,10));
-	if(sub1.equals("IIT") || sub1.equals("LIT") || sub1.equals("IIM") || sub1.equals("BIM") || sub1.equals("ICM") || sub1.equals("ISM") || 		sub1.equals("IIM") || sub1.equals("IHM") || sub1.equals("IRM") || sub1.equals("IWM")){
-	    if(sub2.equals("6") || sub2.equals("5") || sub2.equals("4") || sub2.equals("3")){
-		if(sub3>0 && sub3<=199){		
-        	    this.rollNo=rollNo;	
-		    this.sum=0.0;
-	            this.sumcr=0.0;
-		    return;
+    public student(){
+	   this.sum=0.0;
+	   this.sumcr=0.0;
+    }
+    
+    public void setRollNo(String RollNo) throws InvalidRollNoException{
+        String sub1=RollNo.substring(0,3);
+	    String sub2=RollNo.substring(6,7);
+	    int sub3=Integer.parseInt(rollNo.substring(7,10));
+	    if(sub1.equals("IIT") || sub1.equals("LIT") || sub1.equals("IIM") || sub1.equals("BIM") || sub1.equals("ICM") || sub1.equals("ISM") || 		sub1.equals("IIM") || sub1.equals("IHM") || sub1.equals("IRM") || sub1.equals("IWM")){
+	        if(sub2.equals("6") || sub2.equals("5") || sub2.equals("4") || sub2.equals("3")){
+		        if(sub3>0 && sub3<=199){		
+        	        this.rollNo=RollNo;	
+		            return;
     	        }
-	    }	
+	        }	
         }
-	throw new InvalidRollNoException("Incorrect Roll Number");
+	    throw new InvalidRollNoException("Incorrect Roll Number");
     }
+    
     void setc(int c){
-	this.c=c;
-	this.courses=new course[c];
+	    this.c=c;
+	    this.courses=new course[c];
     }
+    
     String getrollNo(){
         return rollNo;
     }
+    
     public double getCgpa(){
     	for(int i=0;i<c;i++){
-	    sum=sum+courses[i].getCredits()*courses[i].getGrade();
-	    sumcr=sumcr+courses[i].getCredits();
-	}
-	return sum/sumcr;
+	        sum=sum+courses[i].getCredits()*courses[i].getGrade();
+	        sumcr=sumcr+courses[i].getCredits();
+	    }
+	    return sum/sumcr;
     }
+    
     public course getCourse(int j){
-	return courses[j];
+	    return courses[j];
     }
 }
 
 class Batch{
 
-    private student stud[];
-    public Batch(int stud){
-        this.stud=new student[stud];
+    private student[] stud;
+    
+    public Batch(int n){
+        this.stud=new student[n];
+        
     }
     public void st_assign(int i, String rollNo) throws InvalidRollNoException{
-        stud[i]=new student(rollNo);
+        this.stud[i].setRollNo(rollNo);
     }
     student getstudent(int i){
         return stud[i];
@@ -109,28 +118,33 @@ class course{
     private String grade;
     private int credits;
 
-    public course(String courseID) throws InvalidCourseCodeException {
-	String str1=courseID.substring(0,1);
-	int str2=Integer.parseInt(courseID.substring(4,5));
-	int str3=Integer.parseInt(courseID.substring(5,6));
-	int str4=Integer.parseInt(courseID.substring(6,7));
-	String str5=courseID.substring(7,8);
-	if(str1.equals("S") || str1.equals("E") || str1.equals("I") || str1.equals("M")){
-	    if(str2>0 && str2<=9 && str3>=0 && str3<=4 && str4>=0 && str4<=3 ){
-		if(str5.equals("C") || str5.equals("E")){
-        	   this.courseID = courseID;
-        	   this.credits=Integer.parseInt(courseID.substring(5,6))+Integer.parseInt(courseID.substring(6,7));
-		   return ;
-		}
-	    }    
-	}
-	throw new InvalidCourseCodeException("Wrong Course Code");
+    public course(){
+	
     }
+    
+    public void setCourseID(String courseID) throws InvalidCourseCodeException{
+        String str1=courseID.substring(0,1);
+	    int str2=Integer.parseInt(courseID.substring(4,5));
+	    int str3=Integer.parseInt(courseID.substring(5,6));
+	    int str4=Integer.parseInt(courseID.substring(6,7));
+	    String str5=courseID.substring(7,8);
+	    if(str1.equals("S") || str1.equals("E") || str1.equals("I") || str1.equals("M")){
+	        if(str2>0 && str2<=9 && str3>=0 && str3<=4 && str4>=0 && str4<=3 ){
+		        if(str5.equals("C") || str5.equals("E")){
+        	        this.courseID = courseID;
+        	        this.credits=Integer.parseInt(courseID.substring(5,6))+Integer.parseInt(courseID.substring(6,7));
+		            return ;
+		        }
+	        }    
+	    }
+	    throw new InvalidCourseCodeException("Wrong Course Code");
+    }
+    
     public void setGrade(String grade) throws InvalidGradeException{
-	if (!(grade.equals("A+") || grade.equals("A") || grade.equals("B+") || grade.equals("B") || grade.equals("C") || grade.equals("D") || grade.equals("E") || grade.equals("F") || grade.equals("I")))
-	    throw new InvalidGradeException("Illegal Grade");
-	else
-	    this.grade=grade;
+	    if (!(grade.equals("A+") || grade.equals("A") || grade.equals("B+") || grade.equals("B") || grade.equals("C") || grade.equals("D") || grade.equals("E") || grade.equals("F") || grade.equals("I")))
+	        throw new InvalidGradeException("Illegal Grade");
+	    else
+	        this.grade=grade;
     }
 
     public String getCourseID() {
@@ -188,52 +202,60 @@ class display{
 }
 public class Main{
     public static void main(String args[]){
-        int i;
+        int i,flag=0;
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 	    Batch bt=new Batch(n);
         for(i=0;i<n;i++){
-	    try{
-            	String rollNo=sc.nextLine();
-		bt.st_assign(i,rollNo);
-	    }catch(InvalidRollNoException a){
-		System.out.println(a);
-		i--;
-		continue;
-	    }
-	    
-        int m=sc.nextInt();
-	    bt.getstudent(i).setc(m);
-            for(int j=0;j<m;j++){
-		try{
-		    String courseID=sc.nextLine();
-		    bt.getstudent(i).getCourse(j)=new course(courseID);
-		}catch(InvalidCourseCodeException b){
-		    System.out.println(b);
- 		    j--;
+	        try{
+                String rollNo=sc.nextLine();
+		        bt.st_assign(i,rollNo);
+	        }catch(InvalidRollNoException a){
+		    System.out.println(a);
+		    i--;
 		    continue;
-		}LABEL:		
-		try{
-		    String grade=sc.nextLine();
-		    bt.getstudent(i).getCourse(j).setGrade(grade);
-		}catch(InvalidGradeException c){
-		    System.out.println(c);
-		    continue LABEL;
-		}
+	        }
+	    
+            int m=sc.nextInt();
+	        bt.getstudent(i).setc(m);
+            for(int j=0;j<m;j++){
+		        try{
+		            String courseID=sc.nextLine();
+		            bt.getstudent(i).getCourse(j).setCourseID(courseID);
+		        }catch(InvalidCourseCodeException b){
+		            System.out.println(b);
+ 		            j--;
+		            continue;
+		        }
+                while(true){
+                    try{
+                        String grade=sc.nextLine();
+                        bt.getstudent(i).getCourse(j).setGrade(grade);
+                    }catch(InvalidGradeException c){
+                        flag=1;
+                        System.out.println(c);
+                        
+                    }
+                    if(flag==0)
+                        break;
+                    else 
+                        flag=0;
+                }
+            }
         }
-    }
-	int t=sc.nextInt();
-	display dp=new display(n,bt);
-	for(i=0;i<t;i++){
-	    try{
-		String query=sc.nextLine();
-	        dp.setQuery(query);
-	    }catch(InvalidQueryException d){
-		System.out.println(d);
-		i--;
-		continue;
-	    }
-	    dp.show();
-	}
+    
+	    int t=sc.nextInt();
+	    display dp=new display(n,bt);
+	    for(i=0;i<t;i++){
+	        try{
+                String query=sc.nextLine();
+                dp.setQuery(query);
+            }catch(InvalidQueryException d){
+                System.out.println(d);
+                i--;
+                continue;
+            }
+            dp.show();
+	    }                                                                             
     }
 }
